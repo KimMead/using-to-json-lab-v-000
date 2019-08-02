c7a20fe3cab78022c6d08324918055066452fabb
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-08-02 19:13:47 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,7 +27,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @product.to_json(only: [:name, :description, :inventory, :price, :id])}
-    end 
+    end
   end
 
   def data
